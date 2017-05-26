5c2ac0e9fcd8bb49c391ffdf94564d1f7cd6fc1c
dalibor <mail@daliborgogic.com>
2017-05-26 11:18:11 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+/**
+ * Format Currency
+ * @param {String}   currency   ISO 4217:2015 Codes for the representation of currencies
+ * @param {Number}   amount
+ * @return {String}
+ */
+
+function formater(currency, amount) {
+	return new Intl.NumberFormat(undefined, {
+		style: 'currency',
+		currency: currency
+	}).format(amount)
+}
+
+module.exports = formater
