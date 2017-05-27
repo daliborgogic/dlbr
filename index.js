1bc40126bd9401bad7ad165326f5badbb207dffd
dalibor <mail@daliborgogic.com>
2017-05-27 19:57:24 +0200

    github repo added

--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 
 const lang = require('./languages.json')
 
-var Intl = require('intl')
-
-
 /**
  * Format Currency
  * @param {String}   language   'en-US'
@@ -21,11 +18,20 @@ function formatCurrency(language, amount) {
 
   opts.style = 'currency'
   opts.currency = lang[language]['currency']
+
   opts.minimumFractionDigits = lang[language]['minimumFractionDigits']
-
-  let formatter = new Intl.NumberFormat(language,opts)
-
-  return formatter.format(amount)
+  if (typeof window != 'undefined' && window.document) {
+    let formatter = new Intl.NumberFormat(language,opts)
+    return formatter.format(amount)
+  } else {
+    const IntlPolyfill = require('intl')
+    let formatter = new IntlPolyfill.NumberFormat(language,opts)
+    return formatter.format(amount)
+  }
 }
 
 module.exports = formatCurrency
+
+// console.log(formatCurrency('sr-RS', 99.99))
+// console.log(formatCurrency('en-GB', 99.99))
+// console.log(formatCurrency('ja-JP', 99.99))
