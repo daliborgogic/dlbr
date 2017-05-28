696709f206f70821fe240da9f38c79cd76a03412
Karadjordje <sveljod@gmail.com>
2017-05-28 13:52:34 +0200

    made changes to index.js

--- index.js
+++ index.js
@@ -10,18 +10,6 @@ const lang = require('./languages.json')
  */
 
 function formatCurrency(language, amount) {
-<<<<<<< HEAD
-	return new Intl.NumberFormat(language, {
-		style: 'currency',
-<<<<<<< HEAD
-		currency: lang[language]['currency'],
-    	minimumFractionDigits: lang[language]['minimumFractionDigits']
-=======
-		currency: 'RSD',//lang[language]['currency'],
-    minimumFractionDigits: 2 //lang[language]['minimumFractionDigits']
->>>>>>> 2e3f38372148565d9201acc51090ae15f22a6df6
-	}).format(amount)
-=======
   let opts = {}
 
   opts.style = 'currency'
@@ -36,7 +24,6 @@ function formatCurrency(language, amount) {
     let formatter = new IntlPolyfill.NumberFormat(language,opts)
     return formatter.format(amount)
   }
->>>>>>> 06e33636114ac60a449886707ecc1625957e4447
 }
 
 module.exports = formatCurrency
